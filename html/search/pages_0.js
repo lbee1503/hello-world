1ae6ca6fb4f033935c7968926ef216e4e221b6d5
Lbee <jeroen.langeberg@gmail.com>
2022-09-14 10:20:59 +0200

    Added doxygen comments and ran doxygen

---
+++ html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['hello_2dworld_1',['hello-world',['../md_README.html',1,'']]]
+];
--- main.c
+++ main.c
@@ -1,8 +1,22 @@
+/*
+ * @file main.c
+ *
+ * This file contains the main function to start the application
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+
+/** \brief main() is the function that starts the program
+ *
+ * \param int argc is the argument count
+ * \param char **argv is a list of pointer to C-string characters
+ * \return int
+ *
+ */
+int main(int argc, char** argv)
 {
+    // Do something
     printf("Hello world!\n");
     return 0;
 }
